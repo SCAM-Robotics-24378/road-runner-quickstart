6d705deba998866959e42613fc6de493009f7d7e
cadurda <caleb@durda.org>
2025-12-09 21:25:19 -0600

    constants example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SCAM/Constants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.SCAM;
+
+public class Constants {
+
+    //example of using defining common values in a place that can be used by multiple OpModes
+    public static final double FLYWHEEL_TPS_FAR = 1525;
+}
